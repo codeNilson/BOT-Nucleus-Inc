fd02c8a7e0dd10436fb534dd84b185a4132672b8
codenilson <fcodenilson@gmail.com>
2025-05-21 19:29:37 -0300

    Revert "Squash merge feature/add-zoho-library into main"
    This reverts commit 6af59e2ead77606fbb278dca3218b5648ed8d67b.

--- errors/api_connection_error.py
+++
@@ -1,9 +0,0 @@
-class APIConnectionError(Exception):
-    """Custom exception for API connection errors."""
-
-    def __init__(self, message: str):
-        super().__init__(message)
-        self.message = message
-
-    def __str__(self):
-        return f"APIConnectionError: {self.message}"
--- logs/logger.py
+++
@@ -1,8 +0,0 @@
-import os
-import logging
-from dotenv import load_dotenv
-
-load_dotenv()
-
-LOGGER = logging.getLogger("nucleus-inc")
-LOGGER.setLevel(os.environ.get("LOG_LEVEL", "DEBUG"))
